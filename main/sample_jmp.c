ad0434e9a27568a5efb2d3a3e44ea36a6b14e8b4
zouqing <qing.zou@timanetworks.com>
2019-10-25 15:15:19 +0800

    modified sample setjmp

--- main/sample_jmp.c
+++ main/sample_jmp.c
@@ -10,7 +10,114 @@
 #include <unistd.h>
 #include <setjmp.h>
 
-#include "vpk.h"
+
+#if 1
+int max_iteration;
+
+jmp_buf Main;
+jmp_buf PointPing;
+jmp_buf PointPong;
+
+void Ping(void);
+void Pong(void);
+
+void setjmp_test2(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Use %s max-#-of-lines\n", argv[0]);
+        exit(1);
+    }
+    max_iteration = abs(atoi(argv[1]));
+    if (setjmp(Main) == 0)
+        Ping();
+    if (setjmp(Main) == 0)
+        Pong();
+    longjmp(PointPing, 1);
+}
+
+void Ping(void)
+{
+    int i;
+
+    if (setjmp(PointPing) == 0)
+    {
+        i = 1;
+        longjmp(Main, 1);
+    }
+    while (1)
+    {
+        printf("Ping (%2d) - ", i);
+        i++;
+        if (setjmp(PointPing) == 0)
+            longjmp(PointPong, 1);
+    }
+}
+
+void Pong(void)
+{
+    int j;
+
+    if (setjmp(PointPong) == 0)
+    {
+        j = 1;
+        longjmp(Main, 1);
+    }
+    while (1)
+    {
+        printf("Pong (%2d)\n", j);
+        j++;
+        if (j > max_iteration)
+            exit(0);
+        if (setjmp(PointPong) == 0)
+            longjmp(PointPing, 1);
+    }
+}
+#else
+int max_iteration, iter;
+jmp_buf Main, PointA, PointB;
+
+void Ping(void);
+void Pong(void);
+
+void setjmp_test2(int argc, char *argv[])
+{
+    max_iteration = abs(atoi(argv[1]));
+    iter = 1;
+    if (setjmp(Main) == 0)
+        Ping();
+    if (setjmp(Main) == 0)
+        Pong();
+    longjmp(PointA, 1);
+}
+
+void Ping(void)
+{
+    if (setjmp(PointA) == 0)
+        longjmp(Main, 1);
+    while (1)
+    {
+        printf("%3d : Ping-", iter);
+        if (setjmp(PointA) == 0)
+            longjmp(PointB, 1);
+    }
+}
+
+void Pong(void)
+{
+    if (setjmp(PointB) == 0)
+        longjmp(Main, 1);
+    while (1)
+    {
+        printf("Pong\n");
+        iter++;
+        if (iter > max_iteration)
+            exit(0);
+        if (setjmp(PointB) == 0)
+            longjmp(PointA, 1);
+    }
+}
+#endif
 
 jmp_buf buf;
 
@@ -20,7 +127,7 @@ static void foo_call()
     longjmp(buf, 10);
     printf("after foo_call() longjmp \n");
 }
-static int setjmp_test()
+static int setjmp_test1()
 {
     int ret = setjmp(buf);
     if (ret)
@@ -31,16 +138,35 @@ static int setjmp_test()
         foo_call();
     }
 
+    // sleep(1);
+    // printf("re-run\n");
+    // ret = setjmp(buf);
+    // if (ret)
+    //     printf("back from foo_call(), ret = %d\n", ret);
+    // else
+    // {
+    //     printf("before foo_call(), ret = %d\n", ret);
+    //     foo_call();
+    // }
+
+    sleep(1);
+    longjmp(buf, 9);
+
     return 0;
 }
 
 static int main_jump(int argc, char *argv[])
 {
-    setjmp_test();
+    setjmp_test1();
+    printf("\n");
+    setjmp_test2(argc, argv);
+    printf("\n");
+
     return 0;
 }
 
 #ifdef USE_ZLOG
+#include "vpk.h"
 #define SAMPLE_ZLOG_FILE_PATH "."
 #define SAMPLE_ZLOG_CONF_FILE "./zlog.conf"
 int sample_zlog_init(int procname)
