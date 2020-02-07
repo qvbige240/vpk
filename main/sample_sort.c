ed622d9bed490409ce57915d46dcbcc85d082394
zouqing <qing.zou@timanetworks.com>
2020-02-07 11:35:12 +0800

    modified vpk sort

--- main/sample_sort.c
+++ main/sample_sort.c
@@ -20,95 +20,97 @@
 
 static int compare_desc(const void *a, const void *b)
 {
-	return ((int *)a)[0] - ((int *)b)[0];
+    return ((int *)a)[0] - ((int *)b)[0];
 }
 static int compare_asc(const void *a, const void *b)
 {
-	return ((int *)b)[0] - ((int *)a)[0];
+    return ((int *)b)[0] - ((int *)a)[0];
 }
 
 static int test_sort()
 {
-	int nsize = 0, i = 0;
-	int array_num[] = {5, 9, 2, 4, 15, 28, 6, 3, 40, 7, 5, 1, 8};
+    int nsize = 0, i = 0;
+    int array_num[] = {5, 9, 2, 4, 15, 28, 6, 3, 40, 7, 5, 1, 8};
 
-	nsize = _countof(array_num);
-	printf("before sort: \n");
-	for (i = 0; i < nsize; i++)
-		printf("%2d ", array_num[i]);
+    nsize = _countof(array_num);
+    printf("before sort: \n");
+    for (i = 0; i < nsize; i++)
+        printf("%2d ", array_num[i]);
 
-	printf("\nafter desc sort: \n");
-	vpk_ssort(array_num, nsize, sizeof(int), compare_desc);
+    printf("\nafter desc sort: \n");
+    vpk_ssort(array_num, nsize, sizeof(int), compare_desc);
 
-	for (i = 0; i < nsize; i++)
-		printf("%2d ", array_num[i]);
+    for (i = 0; i < nsize; i++)
+        printf("%2d ", array_num[i]);
 
-	printf("\nafter asc sort: \n");
-	vpk_ssort(array_num, nsize, sizeof(int), compare_asc);
+    printf("\nafter asc sort: \n");
+    vpk_ssort(array_num, nsize, sizeof(int), compare_asc);
 
-	for (i = 0; i < nsize; i++)
-		printf("%2d ", array_num[i]);
-	printf("\n");
+    for (i = 0; i < nsize; i++)
+        printf("%2d ", array_num[i]);
+    printf("\n");
 
-	return 0;
+    return 0;
 }
 
 int sort_main(int argc, char *argv[])
 {
- 	//int ret = 0;
- 
- 	//vpk_system_init(argc, argv);
- 	//vpk_logging_level_set("DEBUG");
+    //int ret = 0;
 
-	char* type = "bubble";
-	//if (argc > 1)
-	//{
-	//	type = argv[1];
-	//}
+    //vpk_system_init(argc, argv);
+    //vpk_logging_level_set("DEBUG");
 
-	int o;
-	static const struct option long_options[] = {
-		{ "help",			no_argument,			NULL, 'h' },
-		{ "version",		no_argument,			NULL, 'V' },
-		{ "sample",			required_argument,		NULL, 'd' },
-		{ "type",			required_argument,		NULL, 't' },
-		{ "keycode",		required_argument,		NULL, 'k' },
-		{ "file",			required_argument,		NULL, 'f' },
-		{ "url",			required_argument,		NULL, 'u' },
-		{ "log",			optional_argument,		NULL, 'l' },
-		{ NULL, 0, NULL, 0 }
-	};
+    char *type = "bubble";
+    //if (argc > 1)
+    //{
+    //	type = argv[1];
+    //}
 
-	optind = 1;
-	//LOG_I("22 optind = %d, argc = %d", optind, argc);
-	while ((o = getopt_long(argc, argv, "hVd:t:k:f:u:l", long_options, NULL)) >= 0) {
-		//printf("opt = %c\n", o);  
-		//printf("optarg = %s\n", optarg);  
-		//printf("optind = %d\n", optind);  
-		//printf("argv[optind - 1] = %s\n",  argv[optind - 1]);
-		switch(o) {
-			case 't':
-				type = optarg;
-				break;
-			default:
-				break;
-		}
-	}
+    int o;
+    static const struct option long_options[] = {
+        { "help",        no_argument,            NULL,   'h' },
+        { "version",     no_argument,            NULL,   'V' },
+        { "sample",      required_argument,      NULL,   'd' },
+        { "type",        required_argument,      NULL,   't' },
+        { "keycode",     required_argument,      NULL,   'k' },
+        { "file",        required_argument,      NULL,   'f' },
+        { "url",         required_argument,      NULL,   'u' },
+        { "log",         optional_argument,      NULL,   'l' },
+        { NULL,          0,                      NULL,   0   }
+    };
 
-	LOG_D("type = %s", type);
+    optind = 1;
+    //LOG_I("22 optind = %d, argc = %d", optind, argc);
+    while ((o = getopt_long(argc, argv, "hVd:t:k:f:u:l", long_options, NULL)) >= 0)
+    {
+        //printf("opt = %c\n", o);
+        //printf("optarg = %s\n", optarg);
+        //printf("optind = %d\n", optind);
+        //printf("argv[optind - 1] = %s\n",  argv[optind - 1]);
+        switch (o)
+        {
+        case 't':
+            type = optarg;
+            break;
+        default:
+            break;
+        }
+    }
 
-	double elapsed;
-	struct timeval result, prev, next;	
+    LOG_D("type = %s", type);
 
-	gettimeofday(&prev, 0);
+    double elapsed;
+    struct timeval result, prev, next;
 
-	test_sort();
+    gettimeofday(&prev, 0);
 
-	gettimeofday(&next, 0);
-	vpk_timersub(&next, &prev, &result);
-	//time_sub(&result, &prev, &next);
-	elapsed = result.tv_sec + (result.tv_usec / 1.0e6);
-	LOG_D("vpk time elapsed: %.6f, %d(s) %d(us) \n", elapsed, result.tv_sec, result.tv_usec);
+    test_sort();
 
-	return 0;
+    gettimeofday(&next, 0);
+    vpk_timersub(&next, &prev, &result);
+    //time_sub(&result, &prev, &next);
+    elapsed = result.tv_sec + (result.tv_usec / 1.0e6);
+    LOG_D("vpk time elapsed: %.6f, %d(s) %d(us) \n", elapsed, result.tv_sec, result.tv_usec);
+
+    return 0;
 }
